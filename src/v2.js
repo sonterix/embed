9e9c6a5a225d488c5a58c4926ac9476d2f071920
Nick Bukovskiy <sonterixo@gmail.com>
2022-04-08 11:12:31 +0300

    Embed v2.1

--- src/v2.js
+++ src/v2.js
@@ -1,7 +1,7 @@
 class MoneymadeWidget {
-  hostV1 = 'https://widgets.moneymade.io'
+  static hostV1 = 'https://widgets.moneymade.io'
 
-  hostV2 = 'https://markets.moneymade.io'
+  static hostV2 = 'https://markets.moneymade.io'
 
   node
 
@@ -17,72 +17,46 @@ class MoneymadeWidget {
    * @param {initCallback} [callback] The callback function that fires after all init method is done
    */
   init(callback) {
-    // Create iframe
-    const iframeElement = this.createIframe()
+    let isInited = false
 
-    if (iframeElement) {
-      this.mountIframe(iframeElement)
-      // Send success status
-      if (callback) {
-        callback(this.iframeId, true)
-      }
-    } else {
-      this.hideNode()
-      // Send error status
-      if (callback) {
-        callback(this.iframeId, false)
+    // Function to careate and mount iframe element
+    const renderIframe = () => {
+      if (document.readyState === 'complete') {
+        // Create iframe
+        const iframeElement = this.createIframe()
+
+        if (iframeElement) {
+          this.mountIframe(iframeElement)
+          this.logStatus(true)
+          // Send success status
+          if (callback) {
+            callback(this.iframeId, true)
+          }
+
+          // Change local status
+          isInited = true
+        } else {
+          this.hideNode()
+          this.logStatus(false)
+          // Send error status
+          if (callback) {
+            callback(this.iframeId, false)
+          }
+        }
       }
     }
 
-    // Init listening for the messages from the iframe
-    window.addEventListener('message', this.trackIframeMessages.bind(this))
+    // Init
+    renderIframe()
+
+    // If not inited
+    if (!isInited) {
+      document.addEventListener('readystatechange', renderIframe)
+    }
   }
   /**
    * The callback function that fires after all init method is done
    * @callback initCallback
-   * @param {string} id Iframe id
-   * @param {boolean} status Represents the status of the successful inited widget
-   * @returns {void}
-   */
-
-  /**
-   * Init function. The entry point for the widget functionality
-   * @param {initOnLoadCallback} [callback] The callback function that fires after all init method is done
-   */
-  initOnLoad(callback) {
-    const iframeData = {
-      id: '',
-      status: false
-    }
-
-    // Function to init the iframe creation when the DOM is completely loaded
-    const initOnComplete = () => {
-      if (document.readyState === 'complete') {
-        this.init((id, status) => {
-          iframeData.id = id
-          iframeData.status = status
-        })
-      }
-    }
-
-    if (document.readyState === 'complete') {
-      this.init((id, status) => {
-        iframeData.id = id
-        iframeData.status = status
-      })
-    } else if (!iframeData.status) {
-      document.addEventListener('readystatechange', initOnComplete)
-    } else {
-      document.removeEventListener('readystatechange', initOnComplete)
-    }
-
-    if (callback) {
-      callback(iframeData.id, iframeData.status)
-    }
-  }
-  /**
-   * The callback function that fires after all init method is done
-   * @callback initOnLoadCallback
    * @param {string} id Iframe id
    * @param {boolean} status Represents the status of the successful inited widget
    * @returns {void}
@@ -123,96 +97,96 @@ class MoneymadeWidget {
 
     switch (embedType) {
       case 'investing':
-        url = new URL(`embed-entry/${platformId}`, this.hostV1)
+        url = new URL(`embed-entry/${platformId}`, MoneymadeWidget.hostV1)
         break
 
       case 'calculator':
-        url = new URL('embed-calculator', this.hostV1)
+        url = new URL('embed-calculator', MoneymadeWidget.hostV1)
         break
 
       case 'quizFull':
-        url = new URL('embed-quiz-expand', this.hostV1)
+        url = new URL('embed-quiz-expand', MoneymadeWidget.hostV1)
         break
 
       case 'full':
-        url = new URL('full-experience', this.hostV1)
+        url = new URL('full-experience', MoneymadeWidget.hostV1)
         break
 
       case 'trading':
-        url = new URL(`embed-trading/${platformId}`, this.hostV1)
+        url = new URL(`embed-trading/${platformId}`, MoneymadeWidget.hostV1)
         break
 
       case 'expandableFull':
-        url = new URL('expandable-discovery', this.hostV1)
+        url = new URL('expandable-discovery', MoneymadeWidget.hostV1)
         break
 
       case 'newQuiz':
-        url = new URL('expandable-quiz', this.hostV1)
+        url = new URL('expandable-quiz', MoneymadeWidget.hostV1)
         break
 
       case 'typeWidget':
-        url = new URL('type-widget', this.hostV1)
+        url = new URL('type-widget', MoneymadeWidget.hostV1)
         paramsObj.type = industry
         break
 
       case 'customWidget':
-        url = new URL('custom-widget', this.hostV1)
+        url = new URL('custom-widget', MoneymadeWidget.hostV1)
         paramsObj.type = industry
         break
 
       case 'stockWidget':
-        url = new URL('stock-widget', this.hostV1)
+        url = new URL('stock-widget', MoneymadeWidget.hostV1)
         break
 
       case 'horizontalDiscovery':
-        url = new URL('horizontal-discovery', this.hostV1)
+        url = new URL('horizontal-discovery', MoneymadeWidget.hostV1)
         break
 
       case 'worldwideWidget':
-        url = new URL('worldwide-widget', this.hostV1)
+        url = new URL('worldwide-widget', MoneymadeWidget.hostV1)
         break
 
       case 'promosSlider':
-        url = new URL('promos-slider', this.hostV1)
+        url = new URL('promos-slider', MoneymadeWidget.hostV1)
         break
 
       case 'discoverByInterest':
-        url = new URL('discover-by-interest', this.hostV1)
+        url = new URL('discover-by-interest', MoneymadeWidget.hostV1)
         break
 
       case 'tickerSimple':
-        url = new URL('ticker-simple', this.hostV2)
+        url = new URL('ticker-simple', MoneymadeWidget.hostV2)
         height = height || 230
         break
 
       case 'tickerGraph':
-        url = new URL('ticker-graph', this.hostV2)
+        url = new URL('ticker-graph', MoneymadeWidget.hostV2)
         height = height || 440
         break
 
       case 'articleSnapshot':
-        url = new URL('article-snapshot', this.hostV2)
+        url = new URL('article-snapshot', MoneymadeWidget.hostV2)
         height = height || 360
         break
 
       case 'inlineData':
-        url = new URL('inline-data', this.hostV2)
+        url = new URL('inline-data', MoneymadeWidget.hostV2)
         width = width || 84
         height = height || 24
         break
 
       case 'monetized':
-        url = new URL('monetized', this.hostV2)
+        url = new URL('monetized', MoneymadeWidget.hostV2)
         height = height || 488
         break
 
       case 'categoryRoundup':
-        url = new URL('category-roundup', this.hostV2)
+        url = new URL('category-roundup', MoneymadeWidget.hostV2)
         height = height || 448
         break
 
       case 'tickerTable':
-        url = new URL('ticker-table', this.hostV2)
+        url = new URL('ticker-table', MoneymadeWidget.hostV2)
         height = height || 385
         break
 
@@ -254,56 +228,25 @@ class MoneymadeWidget {
     } else {
       this.node.appendChild(iframeElement)
     }
+
+    // Add class-indicator that iframe is mounted
+    this.node.classList.add('money-made-loaded')
   }
 
   /**
-   * Process through different messages that come from an iframe
-   * @param {MessageEvent} event
+   * Console log widget status
+   * @param {boolean} [status] Status of the current widget
    */
-  trackIframeMessages(event) {
-    if (event.origin === this.hostV1 || event.origin === this.hostV2) {
-      const { action } = event.data
-
-      switch (action) {
-        case 'setWidth': {
-          const { frameId, width } = event.data
-          const iframeElement = document.querySelector(`#${frameId}`)
-
-          if (iframeElement) {
-            iframeElement.setAttribute('width', width)
-          }
-
-          break
-        }
-
-        case 'setHeight': {
-          const { frameId, height } = event.data
-          const iframeElement = document.querySelector(`#${frameId}`)
-
-          if (iframeElement) {
-            iframeElement.setAttribute('height', height)
-          }
-
-          break
-        }
-
-        case 'setSize': {
-          const { frameId, width, height } = event.data
-          const iframeElement = document.querySelector(`#${frameId}`)
-
-          if (iframeElement) {
-            iframeElement.setAttribute('width', width)
-            iframeElement.setAttribute('height', height)
-          }
-
-          break
-        }
-
-        default: {
-          break
-        }
-      }
-    }
+  logStatus(status) {
+    // eslint-disable-next-line no-console
+    console.info(
+      `%cWidget: { id: %c${this.iframeId} %cloaded: %c${status} %c}`,
+      'color: #fff',
+      'color: #6525c2',
+      'color: #fff',
+      `color: ${status ? '#35a577' : '#e62b32'}`,
+      'color: #fff'
+    )
   }
 
   /**
@@ -313,6 +256,95 @@ class MoneymadeWidget {
     this.node[0].style.display = 'none'
     this.node.setAttribute('id', this.iframeId)
     this.node.setAttribute('data-status', 'Failed to load')
+  }
+
+  /**
+   * Process through different messages that come from an iframe
+   */
+  static trackIframeMessages() {
+    // Init listening for the messages from the iframe
+    window.addEventListener('message', event => {
+      if (event.origin === MoneymadeWidget.hostV1 || event.origin === MoneymadeWidget.hostV2) {
+        const { frameId, action, width, height } = event.data
+        const iframeElement = frameId ? document.querySelector(`#${frameId}`) : null
+
+        if (iframeElement) {
+          switch (action) {
+            case 'setWidth': {
+              if (width) {
+                iframeElement.setAttribute('width', width)
+              }
+              break
+            }
+
+            case 'setHeight': {
+              if (height) {
+                iframeElement.setAttribute('height', height)
+              }
+              break
+            }
+
+            case 'setSize': {
+              if (width && height) {
+                iframeElement.setAttribute('width', width)
+                iframeElement.setAttribute('height', height)
+              }
+              break
+            }
+
+            default: {
+              break
+            }
+          }
+        }
+      }
+    })
+  }
+
+  /**
+   * Detect DOM changes that relevant to Moneymade widgets
+   */
+  static trackDOMChanges() {
+    // Create an observer instance linked to the callback function
+    const observer = new MutationObserver(mutationsList => {
+      // Checkign for each mutation
+      mutationsList.forEach(({ type, target, attributeName, addedNodes }) => {
+        // Get class list of current element
+        const classList = target?.classList ? [...target.classList] : []
+        // Detect if it's moneymade element
+        const isMoneymadeEl = classList.includes('money-made-embed')
+        const isMoneymadeElLoaded = classList.includes('money-made-loaded')
+
+        if (isMoneymadeEl) {
+          // If attribute has changed (except 'class') in loaded element
+          if (type === 'attributes' && attributeName !== 'class' && isMoneymadeElLoaded) {
+            target.classList.remove('money-made-loaded')
+            const widget = new MoneymadeWidget(target)
+            widget.init()
+          }
+        } else if (addedNodes.length) {
+          // Checking each added element
+          addedNodes.forEach(element => {
+            const elementClassList = element?.classList ? [...element.classList] : []
+            // If this not loaded moneymade element
+            if (elementClassList.includes('money-made-embed') && !elementClassList.includes('money-made-loaded')) {
+              const widget = new MoneymadeWidget(element)
+              widget.init()
+            }
+          })
+        }
+      })
+    })
+
+    const config = {
+      attributes: true,
+      childList: true,
+      subtree: true,
+      characterData: false
+    }
+
+    // Start observing the document for configured mutations
+    observer.observe(document, config)
   }
 }
 
@@ -324,38 +356,17 @@ class MoneymadeWidget {
 window.mminit = moneymadeEl => {
   // If array of elements
   if (Array.isArray(moneymadeEl)) {
-    const statuses = moneymadeEl.reduce((acc, node) => {
+    moneymadeEl.forEach(element => {
       // Create widget
-      const widget = new MoneymadeWidget(node)
-      widget.initOnLoad((id, status) => {
-        if (status) {
-          node.classList.add('money-made-loaded')
-        }
+      const widget = new MoneymadeWidget(element)
+      return widget.init()
+    })
 
-        acc[id] = status
-      })
-
-      return acc
-    }, {})
-
-    console.table(statuses)
-    return statuses
-  }
-
-  // If single element
-  if (moneymadeEl) {
+    // If single element
+  } else if (moneymadeEl) {
     // Create widget
     const widget = new MoneymadeWidget(moneymadeEl)
-    widget.initOnLoad((id, status) => {
-      if (status) {
-        moneymadeEl.classList.add('money-made-loaded')
-      }
-
-      const data = { [id]: status }
-
-      console.table(data)
-      return data
-    })
+    return widget.init()
   }
 
   return null
@@ -367,43 +378,8 @@ window.addEventListener('load', () => {
   const moneymadeElements = document.querySelectorAll('.money-made-embed:not(.money-made-loaded)')
   // Init widgets inside the elements
   window.mminit([...moneymadeElements])
-
-  // Create an observer instance linked to the callback function
-  const observer = new MutationObserver(mutationsList => {
-    // Checkign for each mutation
-    mutationsList.forEach(({ type, target, attributeName, addedNodes }) => {
-      // Get class list of current element
-      const classList = target?.classList ? [...target.classList] : []
-      // Detect if it's moneymade element
-      const isMoneymadeEl = classList.includes('money-made-embed')
-      const isMoneymadeElLoaded = classList.includes('money-made-loaded')
-
-      if (isMoneymadeEl) {
-        // If attribute has changed (except 'class') in loaded element
-        if (type === 'attributes' && attributeName !== 'class' && isMoneymadeElLoaded) {
-          target.classList.remove('money-made-loaded')
-          window.mminit(target)
-        }
-      } else if (addedNodes.length) {
-        // Checking each added element
-        addedNodes.forEach(element => {
-          const elementClassList = element?.classList ? [...element.classList] : []
-          // If this not loaded moneymade element
-          if (elementClassList.includes('money-made-embed') && !elementClassList.includes('money-made-loaded')) {
-            window.mminit(element)
-          }
-        })
-      }
-    })
-  })
-
-  const config = {
-    attributes: true,
-    childList: true,
-    subtree: true,
-    characterData: false
-  }
-
-  // Start observing the document for configured mutations
-  observer.observe(document, config)
+  // Init track messages from iframe
+  MoneymadeWidget.trackIframeMessages()
+  // Init track DOM changes that relevant to widget
+  MoneymadeWidget.trackDOMChanges()
 })
